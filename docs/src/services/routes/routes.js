0354e3d51a7593a567b9edaf74e3f3c1bb9f0215
Dave Snider <dave.snider@gmail.com>
2017-10-31 14:54:22 -0700

    basics of query panel

--- docs/src/services/routes/routes.js
+++ docs/src/services/routes/routes.js
@@ -74,6 +74,9 @@ import PopoverExample
 import ProgressExample
   from '../../views/progress/progress_example';
 
+import QueryPanelExample
+  from '../../views/query_panel/query_panel_example';
+
 import SideNavExample
   from '../../views/side_nav/side_nav_example';
 
@@ -179,6 +182,9 @@ const components = [{
 }, {
   name: 'Progress',
   component: ProgressExample,
+}, {
+  name: 'QueryPanel',
+  component: QueryPanelExample,
 }, {
   name: 'SideNav',
   component: SideNavExample,
